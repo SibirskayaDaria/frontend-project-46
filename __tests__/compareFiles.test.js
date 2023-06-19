d3256194e0276c113056599f3a6ccc35ceea91e3
DariaSibirskaya <sdaryserg@mail.ru>
2023-06-19 16:15:14 +0300

    fix quotes 4

--- __tests__/compareFiles.test.js
+++ __tests__/compareFiles.test.js
@@ -14,8 +14,9 @@ test('Сравнение плоских файлов JSON - разные зна�
   const file2Path = `${__dirname}/../file2.json`;
 
   const result = compareFiles(file1Path, file2Path);
-  const expected = `- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n`;
+  const expected = '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n';
   expect(result).toEqual(expected);
 });
+
 
 
