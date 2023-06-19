670df7545ef84ca20a4cdfbbcb2d871d4240591a
DariaSibirskaya <sdaryserg@mail.ru>
2023-06-19 19:16:40 +0300

    test

--- __tests__/compareFiles.test.js
+++ __tests__/compareFiles.test.js
@@ -1,17 +1,10 @@
 const { compareFiles } = require('../gendiff.js');
 
-test('Сравнение плоских файлов JSON - одинаковые файлы', () => {
-  const file1Path = `${__dirname}/../file1.json`;
-  const file2Path = `${__dirname}/../file1.json`; // Здесь используется один и тот же файл
-  const result = compareFiles(file1Path, file2Path);
-  const expected = 'follow: false\n  host: hexlet.io\n  proxy: 123.234.53.22\n  timeout: 50\n';
-  expect(result).toEqual(expected);
-});
-
 test('Сравнение плоских файлов JSON - разные значения', () => {
   const file1Path = `${__dirname}/../file1.json`;
   const file2Path = `${__dirname}/../file2.json`;
   const result = compareFiles(file1Path, file2Path);
-  const expected = '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n';
+  const expected = '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true';
   expect(result).toEqual(expected);
 });
+
