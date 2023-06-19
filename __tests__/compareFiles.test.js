36f2bf193cc9e4180be6ba205ede187f5bfecd51
DariaSibirskaya <sdaryserg@mail.ru>
2023-06-19 12:47:05 +0300

    fix gendiff.js

--- __tests__/compareFiles.test.js
+++ __tests__/compareFiles.test.js
@@ -1,13 +1,11 @@
-const { compareFiles } = require('../gendiff');
+const { compareFiles } = require('../gendiff.js');
 
 test('Сравнение плоских файлов JSON - одинаковые файлы', () => {
   const file1Path = `${__dirname}/../file1.json`;
   const file2Path = `${__dirname}/../file1.json`; // Здесь используется один и тот же файл
 
   const result = compareFiles(file1Path, file2Path);
-  const expected = 
-  '  follow: false\n  host: hexlet.io\n  proxy: 123.234.53.22\n  timeout: 50'
-  
+  const expected = 'follow: false\n  host: hexlet.io\n  proxy: 123.234.53.22\n  timeout: 50';
   expect(result).toEqual(expected);
 });
 
