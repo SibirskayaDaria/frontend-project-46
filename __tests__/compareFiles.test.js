e6bfe6cc8f382064c12e484087995f8cd762f84a
DariaSibirskaya <sdaryserg@mail.ru>
2023-07-18 11:28:08 +0300

    block for testing with different formats

--- __tests__/compareFiles.test.js
+++ __tests__/compareFiles.test.js
@@ -2,6 +2,7 @@ import { test, expect, describe } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import { readFileSync } from 'fs';
+import gendiff from '../src/index.js';
 
 import gendiff from '../src/index.js';
 
@@ -12,3 +13,12 @@ const getFixturePath = (filename) => path.join(dirname1, '..', '__fixtures__', f
 const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 const formats = ['json', 'yaml', 'yml'];
+
+describe('gendiff', () => {
+    test.each(formats)('genDiff should work with %p', (format) => {
+      const file1 = getFixturePath(`file1.${format}`);
+      const file2 = getFixturePath(`file2.${format}`);
+  
+      expect(gendiff(file1, file2, 'stylish')).toEqual(readFixture('expectedStylish.txt'));
+    });
+  });
