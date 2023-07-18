24140e105d6c2101e7f372aec5bf0fb6ec2720ed
DariaSibirskaya <sdaryserg@mail.ru>
2023-07-18 11:13:45 +0300

    import module (tests) and path to fixtures

--- __tests__/compareFiles.test.js
+++ __tests__/compareFiles.test.js
@@ -1,9 +1,14 @@
-const { compareFiles } = require('../bin/gendiff.js');
+import { test, expect, describe } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { readFileSync } from 'fs';
 
-test('Сравнение плоских файлов JSON - разные значения', () => {
-  const file1Path = `${__dirname}/../file1.json`;
-  const file2Path = `${__dirname}/../file2.json`;
-  const result = compareFiles(file1Path, file2Path);
-  const expected = '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true';
-  expect(result).toEqual(expected);
-});
+import gendiff from '../src/index.js';
+
+const filename1 = fileURLToPath(import.meta.url);
+const dirname1 = dirname(filename1);
+
+const getFixturePath = (filename) => path.join(dirname1, '..', '__fixtures__', filename);
+const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+const formats = ['json', 'yaml', 'yml'];
