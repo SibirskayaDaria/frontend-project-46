01e1dd7297ed6c763d0e2df4fa1262bcb9310cf5
DariaSibirskaya <sdaryserg@mail.ru>
2023-07-26 15:08:44 +0300

    asciinema

--- __tests__/test.js
+++ __tests__/test.js
@@ -15,10 +15,11 @@ const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8'
 const formats = ['json', 'yaml', 'yml'];
 
 describe('gendiff', () => {
-  test.each(formats)('genDiff should work with %p', (format) => {
+  test.each(formats)('genDiff should work with %p', (format) =>
     const file1 = getFixturePath(`file1.${format}`);
     const file2 = getFixturePath(`file2.${format}`);
 
+    expect(genDiff(file1, file2)).toEqual(readFixture('expectedStylish.txt'))
     expect(gendiff(file1, file2, 'stylish')).toEqual(readFixture('expectedStylish.txt'));
     expect(gendiff(file1, file2, 'plain')).toEqual(readFixture('expectedPlain.txt'));
     expect(gendiff(file1, file2, 'json')).toEqual(readFixture('expectedJSON.txt'));
