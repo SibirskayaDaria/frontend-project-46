4e9a8ef25213da8ae81389afea18b5195f964d85
DariaSibirskaya <sdaryserg@mail.ru>
2023-08-08 21:41:01 +0300

    format

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import { readFileSync } from 'node:fs';
 import parse from './parse.js';
 import getFormat from './getFormat.js';
-import form from './formaters/index.js';
+import format from './formaters/index.js';
 import makeTree from './makeTree.js';
 
 const getFileContent = (path) => readFileSync(path, 'utf-8');
 
-function gendiff(filePath1, filePath2, format = 'stylish') {
+function gendiff(filePath1, filePath2, form = 'stylish') {
   const dataParse1 = parse(getFileContent(filePath1), getFormat(filePath1));
   const dataParse2 = parse(getFileContent(filePath2), getFormat(filePath2));
   const difference = makeTree(dataParse1, dataParse2);
-  return form(difference, format);
+  return format(difference, form);
 }
 
 export default gendiff;
