406cde6f3136642e41731117788e75930a21e2a2
DariaSibirskaya <sdaryserg@mail.ru>
2023-08-08 21:40:55 +0300

    format

--- src/getFormat.js
+++ src/getFormat.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import _ from 'lodash';
 
-const format = (filepath = 'json') => _.trim(path.extname(filepath), '.');
+const form = (filepath = 'json') => _.trim(path.extname(filepath), '.');
 
-export default format;
+export default form;
